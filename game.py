df7311e822199b0a8c732223dcc4a039be515e1e
srahuliitb <rahulsingh@ITRS868s-MacBook-Pro.local>
2020-03-12 12:44:58 +0530

    new files

--- game.py
+++ game.py
@@ -11,23 +11,23 @@ class ShannonExpert:
 
   # update the state of the expert given current sequence element. (selected by user)
   def update(self, current):
-    if self.inputs[0][self.last_2][self.last_1] == current:
-      self.inputs[1][self.last_2][self.last_1] = 1
-      self.inputs[0][self.last_2][self.last_1] = current
+    if self.inputs[self.last_2][self.last_1][0] == current:
+      self.inputs[self.last_2][self.last_1][1] = 1
+      self.inputs[self.last_2][self.last_1][0] = current
 
     else:
-      self.inputs[1][self.last_2][self.last_1] = 0
-      self.inputs[0][self.last_2][self.last_1] = current
-
-    self.last_2 = self.last_1
+      self.inputs[self.last_2][self.last_1][1] = 0
+      self.inputs[self.last_2][self.last_1][0] = current
     self.last_1 = current
+    self.last_2 = self.last_1
+    
 
   # Produce the prediction for the next bit (0 or 1)
   def predict(self):
     pred = 0
 
-    if self.inputs[1][self.last_2][self.last_1] == 1:
-      pred = self.inputs[0][self.last_2][self.last_1]
+    if self.inputs[self.last_2][self.last_1][1] == 1:
+      pred = self.inputs[self.last_2][self.last_1][0]
 
     else:
       pred = random.randint(0, 1)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect, url_for
+from flask import Flask, request, render_template, session, url_for, redirect
 from game import *
 from flask_session import Session
 
@@ -9,6 +9,7 @@ app = Flask(__name__, static_folder='public', template_folder='views')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['SECRET_KEY'] = 'the random string'
+#app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 Session(app)
 
 @app.route("/", methods=["GET", "POST"])
@@ -20,22 +21,38 @@ def start():
             "user_input_list":[],
             "comp_guess_list":[]
         }
-
+    if request.method == "GET":
+        return render_template('index.html', GameDetails=session["gameDetails"])
+    
     if request.method == "POST":
         user_input = int(request.form.get('submit_button'))
         session["gameDetails"]['user_input_list'].append(user_input)
         comp_guess = mind_reader.predict()
         session["gameDetails"]['comp_guess_list'].append(comp_guess)
-
+        
         current = user_input
         mind_reader.update(current)
-
-        if comp_guess == user_input:
-         session["gameDetails"]['comp_score'] += 1
+        
+        if user_input == comp_guess: 
+            session["gameDetails"]['comp_score'] += 1     
+        
         else:
-          session["gameDetails"]['user_score'] += 1
+            session["gameDetails"]['user_score'] += 1
 
-    return render_template('index.html', GameDetails=session["gameDetails"])
+        return redirect (url_for('start'))
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 
 
 @app.route('/reset')
@@ -48,5 +65,5 @@ def reset():
 
 
 if (__name__ == "__main__"):
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
 
